32617f0c31dc784b54eccd1a47f4fa90a07a2028
Lijb <lijb1121@163.com>
2018-11-25 22:33:00 +0800

    this is springcloud

---
+++ Eureka/src/main/java/com/baizhi/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        System.out.println("注册中心已经启动");
+
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
